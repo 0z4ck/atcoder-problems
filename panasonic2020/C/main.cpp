312505b5317b574af73d9a292cea6d65f52d058d
0z4ck <z4ck@m.z4ck.tk>
2022-01-03 15:09:16 +0100

    another atempt

--- panasonic2020/C/main.cpp
+++ panasonic2020/C/main.cpp
@@ -23,6 +23,10 @@ const string YES = "Yes";
 const string NO = "No";
 
 void solve(long long a, long long b, long long c){
+    if ( a+b+2*sqrt(a*b)<c)
+        cout<<YES<<endl;
+    else
+        cout<<NO<<endl;
 
 }
 
