f56c828cd8078fb8d20390426672f4a15792b7ac
0z4ck <z4ck@m.z4ck.tk>
2022-01-03 16:08:59 +0100

    another atempt

--- panasonic2020/C/main.cpp
+++ panasonic2020/C/main.cpp
@@ -23,7 +23,7 @@ const string YES = "Yes";
 const string NO = "No";
 
 void solve(long long a, long long b, long long c){
-    if ( a+b+sqrt(4*a*b)<c)
+    if (4*a*b<(c-a-b)*(c-a-b))
         cout<<YES<<endl;
     else
         cout<<NO<<endl;
