a14309b3037f68c2874451197386a3ab3d41eee7
0z4ck <z4ck@m.z4ck.tk>
2022-01-30 15:12:23 +0100

    another atempt

--- abc237/C/main.cpp
+++ abc237/C/main.cpp
@@ -28,7 +28,7 @@ void solve(std::string S){
         return;
     }
 
-    for(int i=h_a;i<N;i++){
+    for(int i=h_a;i<N-t_a;i++){
         if (S[i]!=S[N-i-1+h_a-t_a]){
             cout<<NO<<endl;
             return ;
