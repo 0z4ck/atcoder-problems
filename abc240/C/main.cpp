82841130b51d14a72685e0bc15b2e448b33d2102
0z4ck <z4ck@m.z4ck.tk>
2022-02-20 13:25:48 +0100

    another atempt

--- abc240/C/main.cpp
+++ abc240/C/main.cpp
@@ -60,8 +60,8 @@ const string NO = "No";
 void solve(long long N, long long X, std::vector<long long> a, std::vector<long long> b){
     vector<bool> dp(10200,false);
     dp[0]=true;
-    for(int i=0;i<X+1;i++){
-        for(int j=1;j<N;j++){
+    for(int i=1;i<X+1;i++){
+        for(int j=0;j<N;j++){
             if(i>a[j])
                 dp[i]=dp[i-a[j]];
             if(i>b[j])
