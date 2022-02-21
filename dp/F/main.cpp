7d34bcd109afb87f5b00ccbc1095437463634329
0z4ck <z4ck@m.z4ck.tk>
2022-02-21 22:58:52 +0100

    another atempt

--- dp/F/main.cpp
+++ dp/F/main.cpp
@@ -63,7 +63,7 @@ void solve(std::string s, std::string t){
     for(int i=1;i<s.size()+1;i++)
         for(int j=1;j<t.size()+1;j++){
             dp[i][j] = max(dp[i][j-1], dp[i-1][j]);
-            if (s[i]==t[i])
+            if (s[i-1]==t[i-1])
                 dp[i][j] = max(dp[i][j], dp[i-1][j-1]+1);
         }
     cerr<<dp<<endl;
