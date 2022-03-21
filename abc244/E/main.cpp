de4568e9ba61e7661672a900633b9dd3c8ce2cef
0z4ck <z4ck@m.z4ck.tk>
2022-03-21 21:30:53 +0100

    another atempt

--- abc244/E/main.cpp
+++ abc244/E/main.cpp
@@ -99,7 +99,7 @@ void solve(long long N, long long M, long long K, long long S, long long T, long
     rep(i,K-1)
         rep(j,N)
             for(auto &a: G[j])
-                if(a==X-1){
+                if(j==X-1){
                     dp[i+2][j][0] = (dp[i+2][j][0]+dp[i+1][a][1])%MOD;
                     dp[i+2][j][1] = (dp[i+2][j][1]+dp[i+1][a][0])%MOD;
                 }else{
