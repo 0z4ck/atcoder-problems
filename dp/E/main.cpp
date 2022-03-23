adb1fe152eb3fb5cf78d126f60bd6fae7077bbdf
0z4ck <z4ck@m.z4ck.tk>
2022-03-23 20:07:17 +0100

    another atempt

--- dp/E/main.cpp
+++ dp/E/main.cpp
@@ -62,7 +62,7 @@ void solve(long long N, long long W, std::vector<long long> w, std::vector<long 
         for(int val=100000;val>0;val--)
             if (val-v[i]>=0)
                 dp[val] = min(dp[val-v[i]]+w[i],min(dp[val],dp[val+1]));
-    cerr<<dp;
+    cerr<<(vector<long long>(dp.begin(), dp.begin()+100));
     int ans = 100000;
     while(dp[ans]>W)ans--;
     cout<<ans<<endl;
