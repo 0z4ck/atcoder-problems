cd8d01372afd16508cf25666ab58b3d452c3c7a5
0z4ck <z4ck@m.z4ck.tk>
2022-04-16 15:33:16 +0200

    another atempt

--- abc248/C/main.cpp
+++ abc248/C/main.cpp
@@ -6,7 +6,7 @@
 #define rall(obj) (obj).rbegin(),(obj).rend()
 typedef long long ll;
 using namespace std;
-const int dx[4] = {1,0,-1,0}, dy[4] = {0,1,0,-1};
+const long long dx[4] = {1,0,-1,0}, dy[4] = {0,1,0,-1};
 
 template<typename T>
 std::ostream& operator<<(std::ostream& s, const std::vector<T>& v){
@@ -86,8 +86,8 @@ void solve(long long N, long long M, long long K){
     //vector<vector<vector<long long>>> dp(N+1, vector<vector<long long>>(N,vector<long long>(K+1,0)));
     vector<vector<long long>> dp(N,vector<long long>(K+1,0));
     
-    for(int acc=1;acc<N;acc++){
-        for(int i=1;i<M+1;i++)
+    for(long long acc=1;acc<N;acc++){
+        for(long long i=1;i<M+1;i++)
             dp[0][i]=1;
             //dp[acc][0][i]=1;
     
@@ -99,18 +99,18 @@ void solve(long long N, long long M, long long K){
     
         */
     
-        for(int i=1;i<N;i++){
-            for(int j=i+1;j<K+1;j++){
-                for(int k=i;k<j&&k-i<M;k++){
-                    //DBG(i);DBG(j);DBG(k);DBG(dp[i][j]);DBGln(dp[i-1][k]);
+        for(long long i=1;i<N;i++){
+            for(long long j=i+1;j<K+1;j++){
+                for(long long k=max(K-M,i);k<j&&k-i<M;k++){
+                    DBG(i);DBG(j);DBG(k);DBG(dp[i][j]);DBGln(dp[i-1][k]);
                     dp[i][j] = (dp[i][j] + dp[i-1][k]) %MOD;
                     //dp[acc][i][j] = (dp[acc][i][j] + dp[acc][i-1][k]) %MOD;
                 }
             }
         }
     }
-    int ans=0;
-    for(int acc=1;acc<K+1;acc++)
+    long long ans=0;
+    for(long long acc=1;acc<K+1;acc++)
         ans = (ans + dp[N-1][acc])%MOD;
     //cout<<dp[N-1][K]<<endl;
     cout<<ans<<endl;
