4ec1eabc8d4cefe75ec38f4e19e9805146379145
0z4ck <z4ck@m.z4ck.tk>
2022-04-24 14:43:11 +0200

    another atempt

--- arc139/A/main.cpp
+++ arc139/A/main.cpp
@@ -91,13 +91,13 @@ void solve(long long N, std::vector<long long> T){
         }else if (ctz>T[i]){
             a = a | one<<T[i];
             ctz = T[i];
-        }else if(ctz<T[i]){
+        }else if(ctz<T[i]&&false){
             a >> T[i];
             a++;
             a << T[i];
             a = a | one<<T[i];
             ctz = T[i];
-        }else if(ctz==T[i]){
+        }else if(ctz<=T[i]){
             a >> (T[i]+1);
             a++;
             a << (T[i]+1);
